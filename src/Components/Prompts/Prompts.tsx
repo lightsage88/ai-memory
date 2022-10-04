fda7b0d79ef47114cdcfc9e35cc5688f4b3f1b93
Adrian <adrian.e.rosales@gmail.com>
2022-10-04 05:41:02 -0700

    created input modal for AI to get prompts to create images from once request is made via API

--- src/Components/Prompts/Prompts.tsx
+++ src/Components/Prompts/Prompts.tsx
@@ -1,14 +1,72 @@
-import { useState } from 'react';
+import { ReactElement, useState } from "react";
+import StyledComponents from "../../StyledComponents/StyledComponents";
+import { Button, TextField } from "@mui/material";
+import $ from 'jquery';
 
 export const Prompts = () => {
-    const [prompt, setPrompts] = useState<String[]>([]);
+  const [prompts, setPrompts] = useState<any[]>([]);
 
-    return (
-        <div id="prompts-div" data-testid="prompts-div">
-            <h1>Prompts goes here</h1>
-            
-        </div>
-    )
-}
+  /**
+   * This method handles the change in a textfield and sets the prompts within the component's useState implementation
+   * for 'prompts'.
+   * @param value {String} - the input from the user
+   * @param id {Number} - the number representing the 'id' for the prompt object to be
+   */
+  const handleTextFieldChange = (value: string, id: number) => {
+    setPrompts([
+      { prompt: value, id },
+      ...prompts.filter((el) => el.id !== id),
+    ]);
+  };
+
+  const resetPrompts = () => {
+    setPrompts([]);
+    const inputs = document.getElementsByClassName('text-field-for-prompt');
+    $('.MuiInputBase-input').val('');
+  };
+
+  /**
+   * This method returns the JSX to render the textFields
+   * @returns {JSX}
+   */
+  const textFields = () => {
+    let jsxToReturn = [];
+    for (let i = 0; i < 8; i++) {
+      const randomId = Math.ceil(Math.random() * 100000);
+      jsxToReturn.push(
+        <TextField
+          className="text-field-for-prompt"
+          key={i}
+          variant="outlined"
+          onChange={(e: any) => handleTextFieldChange(e.target.value, i)}
+        ></TextField>
+      );
+    }
+    return jsxToReturn;
+  };
+
+  const determineContinueButtonDisability = () => {
+    if (
+      prompts.length === 8 &&
+      prompts.filter((el) => el.prompt === "").length === 0
+    ) {
+      return false;
+    }
+    return true;
+  };
+  return (
+    <StyledComponents.StyledPromptsDialog open={true}>
+      <h3>Enter 8 descriptions for the AI to make pictures from.</h3>
+      <p>
+        <strong>Example: </strong>Starfox helping Ukraine fight Putin
+      </p>
+      {textFields()}
+      <Button disabled={determineContinueButtonDisability()}>
+        Let's Rock!
+      </Button>
+      <Button onClick={() => resetPrompts()}>Clear</Button>
+    </StyledComponents.StyledPromptsDialog>
+  );
+};
 
 export default Prompts;
--- src/StyledComponents/StyledComponents.tsx
+++ src/StyledComponents/StyledComponents.tsx
@@ -1,13 +1,25 @@
-import styled from 'styled-components';
-import { Link } from '@mui/material';
+import styled from "styled-components";
+import { Link, Dialog } from "@mui/material";
 
 const StyledNavLink = styled(Link)`
-    margin-left: 1rem;
-    margin-right: 1rem;
+  margin-left: 1rem;
+  margin-right: 1rem;
+`;
+
+const StyledPromptsDialog = styled(Dialog)`
+  .MuiPaper-root {
+    padding: 2rem;
+    border: solid red 3px;
+
+    div.MuiFormControl-root {
+        margin: 0.33rem;
+    }
+  }
 `;
 
 const StyledComponents = {
-    StyledNavLink
+  StyledNavLink,
+  StyledPromptsDialog,
 };
 
 export default StyledComponents;
